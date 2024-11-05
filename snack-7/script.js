7db21caf6b53cc6cbbc5f5d749176eb2dae5c624
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2024-11-05 17:52:37 +0100

    completato esercizio

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const studente2 = students.find((studente) => studente.id === 2);
+console.log("studente2: ", studente2);
+
+
--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const marco = students.find(studente => studente.name === "Marco Lanci");
+console.log("classe di marco: ", marco.class);
